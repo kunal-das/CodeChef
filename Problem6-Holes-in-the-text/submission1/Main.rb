cc5237d2d8fbe7d0d90f4c731d31942496530297
Kunal Das <kunaldasece@gmail.com>
2014-08-15 16:55:11 +0530

    CodeChef Problem 6 - Holes in the text
    this solution is for the problem for finding the number of holes in the file
    for characters 'A', 'D', 'O', 'P', 'Q', 'R' the number of holes is 1 and for 'B' is 2
    the first line of input contains the number of test cases followed by the words in the 
    capital letter.

---
+++ Problem6-Holes-in-the-text/submission1/Main.rb
@@ -0,0 +1,34 @@
+
+def get_string(no_of_times)
+	1.upto(no_of_times) do 
+	string_input = gets.chomp
+	count_holes(string_input)
+	end	
+end
+
+def count_holes(word)
+	count = 0
+	word.each_char do |char|
+		if ["A","D","O","P","Q","R"].include?(char)
+			count += 1
+		elsif char == "B"
+			count += 2
+		end
+	end
+	$arr_of_holes << count
+end
+
+
+
+#puts "enter the number of test cases"
+test_cases = gets.chomp.to_i
+
+$arr_of_holes = []
+get_string(test_cases)
+puts $arr_of_holes
+
+
+
+
+
+
