eae067d931ee339914cb73fc77ce6f4593e18bc4
Kunal Das <kunaldasece@gmail.com>
2014-09-07 16:38:07 +0530

    Holes In The Text

--- Problem6/submission1/Main.rb
+++ Problem6/submission1/Main.rb
@@ -1,34 +1,34 @@
 
-def get_string(no_of_times)
-	1.upto(no_of_times) do 
-	string_input = gets.chomp
-	count_holes(string_input)
-	end	
-end
+## HOLES IN THE TEXT ##
 
-def count_holes(word)
-	count = 0
-	word.each_char do |char|
-		if ["A","D","O","P","Q","R"].include?(char)
-			count += 1
-		elsif char == "B"
-			count += 2
-		end
+class Main
+
+	attr_reader :arr_of_holes
+	
+	def initialize
+		@arr_of_holes = []
 	end
-	$arr_of_holes << count
+
+	def get_string(no_of_times)
+		1.upto(no_of_times) do
+			string_input = gets.chomp
+			count_holes(string_input)
+		end
+		puts arr_of_holes
+	end
+	def count_holes(word)
+		count = 0
+		word.each_char do |char|
+			if ["A","D","O","P","Q","R"].include?(char)
+				count += 1
+			elsif char == "B"
+				count += 2
+			end
+		end
+		@arr_of_holes << count
+	end
 end
 
-
-
-#puts "enter the number of test cases"
+run = Main.new
 test_cases = gets.chomp.to_i
-
-$arr_of_holes = []
-get_string(test_cases)
-puts $arr_of_holes
-
-
-
-
-
-
+run.get_string(test_cases)
