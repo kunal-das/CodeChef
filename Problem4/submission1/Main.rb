4174ed241ae8bc61a9a4a053a51b86b705e93863
Kunal Das <kunaldasece@gmail.com>
2014-09-07 16:27:17 +0530

    Factorial

--- Problem4/submission1/Main.rb
+++ Problem4/submission1/Main.rb
@@ -1,20 +1,31 @@
+
+##FACTORIAL##
+
+class Main
+attr_reader :zeroes_count
 def factorial_input(user_input)
 	fact_arr = []
 	user_input.times do |;input_num|
 		input_num = gets.chomp.to_i
-		fact_arr << trailing_zeroes(input_num)
+		fact_arr << input_num
 	end
 	fact_arr
 end
-def trailing_zeroes(num)
-	divisor = 5
-	zeroes_count = 0
-	while true
-		zeroes_count += num / divisor
-		break if divisor*5>num
-		divisor = divisor*5
+def trailing_zeroes(arr)
+	arr.each do |num|
+		divisor = 5
+		@zeroes_count = 0
+		while true
+			@zeroes_count += num / divisor
+			break if divisor*5>num
+			divisor = divisor*5
+		end
+		@zeroes_count
+		#puts zeroes_count
 	end
-	zeroes_count
 end
+end
+run = Main.new
 user_input = gets.chomp
-puts factorial_input(user_input.to_i)
+arr = run.factorial_input(user_input.to_i)
+run.trailing_zeroes(arr)
