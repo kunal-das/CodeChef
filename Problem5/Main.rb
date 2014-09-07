e61bab672d46d3316c2c2f432958a43e86334b7d
Kunal Das <kunaldasece@gmail.com>
2014-09-07 16:30:53 +0530

    Small Factorials

--- Problem5/Main.rb
+++ Problem5/Main.rb
@@ -1,15 +1,22 @@
-def get_numbers(user_input)
-	arr = []
-	user_input.times do |;input_num|
+
+## SMALL FACTORIALS ##
+
+class Main
+	attr_reader :arr
+	def get_numbers(user_input)
+	@arr = []
+		user_input.times do |;input_num|
 		input_num = gets.chomp.to_i
 		arr << factorial(input_num)
+		end
+		puts arr
 	end
-	puts arr
+
+	def factorial(num)
+		num == 0 ? 1 : num * factorial(num-1)
+	end
 end
 
-def factorial(num)
-	num == 0 ? 1 : num * factorial(num-1)
-end
-
+run = Main.new
 user_input = gets.to_i
-get_numbers(user_input)
+run.get_numbers(user_input)
