3b1debcd0064783a580f17c97218e40d57b6b827
Kunal Das <kunaldasece@gmail.com>
2014-09-07 16:17:21 +0530

    Create prob6_spec.rb

---
+++ spec/prob6_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+require_relative '../Problem6/submission1/Main.rb'
+
+describe "Main" do
+	let(:run) { Main.new }
+
+	it "should have an empty array" do
+		expect(run.arr_of_holes).to be_empty
+	end
+
+	it "should run the specified number of times" do
+		Main.any_instance.stub_chain(:gets, :chomp).and_return("APPLE")
+		run.get_string(2)
+		expect(run.arr_of_holes.length).to eql(2)
+	end
+
+	it "should return the nummber of holes in a word" do
+		run.count_holes("APPLE")
+		expect(run.arr_of_holes).to eql([3])
+	end
+end
