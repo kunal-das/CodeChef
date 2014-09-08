b86b68993bd9886515df7db021e06ad7246abb1a
Kunal Das <kunaldasece@gmail.com>
2014-09-08 17:33:43 +0530

    Spec for Factorials

--- spec/prob4_submission1_spec.rb
+++ spec/prob4_submission1_spec.rb
@@ -1,13 +1,27 @@
 ## SPECS FOR THE FACTORIAL PROBLEM##
 
 require 'spec_helper'
-require_relative '../Problem4/submission 1/Main.rb'
+require_relative '../lib/problem4.rb'
 
 describe "Main" do
-	let(:run) {Main.new}
+	let(:run) { Main.new }
 
-	it "should return the number of trailing zeroes in array" do
-	run.trailing_zeroes([60])
-	expect(run.zeroes_count).to eql(14)
+	it "should prompt the user equal to test cases" do
+		tests = 2
+		Main.any_instance.stub_chain(:gets, :chomp, :to_i).and_return(rand(1..100))
+		expect(run.array_of_input_numbers(tests).length).to eql(2)
+	end
+
+	it "should return an array of input numbers" do
+		Main.any_instance.stub_chain(:gets, :chomp, :to_i).and_return(rand(1..100))
+		expect(run.array_of_input_numbers(rand(1..100)).class).to eql(Array) 
+	end
+
+	it "should return the trailing zeroes" do
+		expect(run.trailing_zeroes(60)).to eql(14)
+	end
+
+	it "should return array of trailing zeroes" do
+		expect(run.returns_array_of_trailing_zeroes([3, 60])).to eql([0, 14])
 	end
 end
