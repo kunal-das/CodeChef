1cae3661ec0e3b4b4cdee69883c4f7e74556db74
Kunal Das <kunaldasece@gmail.com>
2014-09-08 18:45:35 +0530

    spec for holes in the text

--- spec/prob6_spec.rb
+++ spec/prob6_spec.rb
@@ -2,23 +2,33 @@
 ## SPEC FOR THE HOLES IN THE TEXT ##
 
 require 'spec_helper'
-require_relative '../Problem6/submission1/Main.rb'
+require_relative '../lib/problem6.rb'
 
 describe "Main" do
+	
 	let(:run) { Main.new }
 
-	it "should have an empty array" do
-		expect(run.arr_of_holes).to be_empty
-	end
-
-	it "should run the specified number of times" do
+	it "should prompt the user equal to test cases" do
+		tests = 2
 		Main.any_instance.stub_chain(:gets, :chomp).and_return("APPLE")
-		run.get_string(2)
-		expect(run.arr_of_holes.length).to eql(2)
+		expect(run.array_of_words(tests).length).to eql(2)
 	end
 
-	it "should return the nummber of holes in a word" do
-		run.count_holes("APPLE")
-		expect(run.arr_of_holes).to eql([3])
+	it "should return an array of words" do
+		Main.any_instance.stub_chain(:gets, :chomp).and_return("APPLE")
+		expect(run.array_of_words(rand(1..100)).class).to eql(Array)
+		expect(run.array_of_words(rand(1..100)).first).to eql("APPLE") 
+	end
+
+	it "should return a zero count for empty string" do
+		expect(run.count_holes(" ")).to eql(0)
+	end
+
+	it "should return non zero count for non-empty string" do
+		expect(run.count_holes("APPLE")).to eql(3)
+	end
+
+	it "should return array of hole count" do
+		expect(run.array_of_hole_counts(["APPLE", "BALL"])).to eql([3, 3])
 	end
 end
