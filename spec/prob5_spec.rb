adb7dea28db96c82d84fde5c4c3076b61d35689d
Kunal Das <kunaldasece@gmail.com>
2014-09-08 16:52:58 +0530

    Spec for Small Factorials

--- spec/prob5_spec.rb
+++ spec/prob5_spec.rb
@@ -2,18 +2,30 @@
 ## SPEC FOR THE SAMLL FACTORIAL PROBLEM ##
 
 require 'spec_helper'
-require_relative '../Problem5/Main.rb'
+require_relative '../lib/problem5.rb'
 
 describe "Main" do
-	let(:run) {Main.new}
+	
+	let(:run) { Main.new }
 
-	it "should return the factorial of the number" do
-		expect(run.factorial(3)).to eql(6)
+	it "should return factorial" do
+		#run = Main.new
+		expect(run.factorial(4)).to eql(24)
 	end
 
-	it "runs only 2 times" do
-		Main.any_instance.stub_chain(:gets, :chomp, :to_i).and_return(3)
-		run.get_numbers(2)
-		expect(run.arr.length).to eql(2)
+	it "should return an array of input numbers" do
+		#run = Main.new
+		Main.any_instance.stub_chain(:gets, :chomp, :to_i).and_return(rand(1..100))
+		expect(run.get_numbers(rand(1..100)).class).to eql(Array) 
+	end
+
+	it "should prompt the user equal to test cases" do
+		#run = Main.new
+		Main.any_instance.stub_chain(:gets, :chomp, :to_i).and_return(rand(1..100))
+		expect(run.get_numbers(2).length).to eql(2)
+	end
+
+	it "should return an array of factorials" do
+		expect(run.array_of_factorial([3, 2]).first).to eql(6)
 	end
 end
