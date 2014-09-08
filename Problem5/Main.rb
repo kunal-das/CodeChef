d1ce83ba739ef8e74e04ad21e3e2113c9752288e
Kunal Das <kunaldasece@gmail.com>
2014-09-08 16:51:03 +0530

    Update Main.rb

--- Problem5/Main.rb
+++ Problem5/Main.rb
@@ -2,21 +2,32 @@
 ## SMALL FACTORIALS ##
 
 class Main
-	attr_reader :arr
-	def get_numbers(user_input)
-	@arr = []
-		user_input.times do |;input_num|
-		input_num = gets.chomp.to_i
-		arr << factorial(input_num)
+
+	attr_reader :test_case, :arr_of_input_numbers
+
+	def get_numbers(num)
+		@arr_of_input_numbers = []
+		num.times do |;input_num|
+		  input_num = gets.chomp.to_i
+		  @arr_of_input_numbers << input_num
 		end
-		puts arr
+		@arr_of_input_numbers
 	end
 
 	def factorial(num)
 		num == 0 ? 1 : num * factorial(num-1)
 	end
+
+	def array_of_factorial(arr)
+		arr_of_factorials = []
+		arr.each do |num|
+			arr_of_factorials << factorial(num)
+		end
+		arr_of_factorials
+	end
 end
 
 run = Main.new
-user_input = gets.to_i
-run.get_numbers(user_input)
+test_case = gets.chomp.to_i
+run.get_numbers(test_case)
+puts run.array_of_factorial(run.arr_of_input_numbers)
